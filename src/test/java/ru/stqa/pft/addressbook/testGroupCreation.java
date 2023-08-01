9f3414d45cc46d4b32eaee0e406be0b2f83f4348
Herandnu <tanya.rfdm@hotmail.com>
2023-08-01 21:29:24 +0300

    created TestBase class

--- src/test/java/ru/stqa/pft/addressbook/testGroupCreation.java
+++ src/test/java/ru/stqa/pft/addressbook/testGroupCreation.java
@@ -1,37 +1,10 @@
 package ru.stqa.pft.addressbook;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.HashMap;
-import java.util.Map;
+public class testGroupCreation extends TestBase {
 
-public class testGroupCreation {
-  private WebDriver driver;
-  JavascriptExecutor js;
-
-
-
-  @Before
-  public void setUp() {
-    driver = new ChromeDriver();
-    js = (JavascriptExecutor) driver;
-    Map<String, Object> vars = new HashMap<String, Object>();
-    driver.get("http://localhost/addressbook/addressbook/");
-    driver.manage().window().setSize(new Dimension(1550, 838));
-    driver.findElement(By.name("user")).click();
-    driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.id("LoginForm")).click();
-    driver.findElement(By.name("pass")).click();
-    driver.findElement(By.name("pass")).sendKeys("secret");
-    driver.findElement(By.cssSelector("input:nth-child(7)")).click();
-  }
 
   @Test
   public void testGroupCreation() {
@@ -47,8 +20,4 @@ public class testGroupCreation {
     driver.findElement(By.linkText("group page")).click();
   }
 
-  @After
-  public void tearDown() {
-    driver.quit();
-  }
 }
